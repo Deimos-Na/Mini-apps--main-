78a7e41a0405e36388774a6ac0b4bcadcabef1cf
Deimos-Na <deeimoss@yandex.ru>
2024-09-04 18:20:16 +0300

    выполнены все мини-задания

---
+++ js/main.js
@@ -0,0 +1,53 @@
+// let a = 10
+// alert(a)
+
+// let iphone = 2007
+// alert(iphone)
+
+// let nameJS = 'Брендан Эйк'
+// alert(nameJS)
+
+// let a = 10
+// let b = 2
+// alert(a + b)
+// alert(a - b)
+// alert(a * b)
+// alert(a / b)
+
+// let result = 2 ** 5
+// alert(result)
+
+// let a = 9
+// let b = 2
+// alert(a % b)
+
+// let num = 1
+// num += 5
+// num -= 3
+// num *= 7
+// num /= 3
+// num++
+// num--
+// alert(num)
+
+// let age = prompt('Сколько вам лет?')
+// alert(age)
+
+// let user = {
+//   name: 'Nastya',
+//   age: 29,
+//   isAdmin: true,
+// }
+
+// let userName = prompt('Ваше имя?')
+// alert(`Привет, ${userName}`)
+
+let number = prompt('Загадайте любое число')
+let numberQ = number * 2
+alert(numberQ)
+let numberW = numberQ + 10
+alert(numberW)
+let numberE = numberW / 2
+alert(numberE)
+let numberItog = numberE - number
+alert(numberItog)
